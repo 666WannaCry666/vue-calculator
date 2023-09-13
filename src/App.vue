63505840343306cc4d85224eadf4dad3d4a3d024
666WannaCry666 <usov3124@gmail.com>
2023-09-13 22:15:30 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="App">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
